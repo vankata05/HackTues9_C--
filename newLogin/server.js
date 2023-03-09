3a5351f4f9ea500066f095b70fe8a7bd5648cbb3
KNProNoob <kris.nikolov07@gmail.com>
2023-03-09 17:37:50 +0200

    Auth code added

--- newLogin/server.js
+++ newLogin/server.js
@@ -5,7 +5,10 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const Validator = require('validator')
 app.use(express.urlencoded({ extended: true }));
-
+var nodemailer = require('nodemailer');
+let email1;
+let hash1;
+let auth_code;
 
 //Connect to DB
 async function connectdb() {
@@ -60,14 +63,57 @@ app.post("/register", async (req, res) => {
     }
     else
     {
-        
-        hash = await bcrypt.hash(data.password, 10)
-        let User = new UserModel({email: data.email, password: hash})
-        User.save()
-        res.end("User Created")
+        email1 = data.email
+        hash1 = await bcrypt.hash(data.password, 10)
+        email_authorization()
+        fs.readFile("site/authorization.html", (err, data) => {
+            res.end(data)
+        })
     }
 
 });
+
+async function email_authorization()
+{
+    auth_code = Math.floor(100000 + Math.random() * 900000)
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'petsafe1234@gmail.com',
+            pass: 'tjggczeipbzkxutn'
+        }
+    });
+
+    let mailOptions = {
+    from: 'petsafe1234@gmail.com',
+    to: email1,
+    subject: 'Authorization Code',
+    text: auth_code.toString()
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+    });
+}
+
+//Auth page
+
+app.post("/authorization", async(req, res) => {
+    if(req.body.auth_code == auth_code)
+    {
+        let User = new UserModel({email: email1, password: hash1})
+        await User.save()
+        res.end("Registration Done")
+    }
+    else
+    {
+        res.end("Wrong Code")
+    }
+})
 
 //Login
 app.post("/login", async (req, res) => {
